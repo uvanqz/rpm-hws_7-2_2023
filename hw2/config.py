792f7319176fba85e7564f999f1299afe9eb068d
Angelina Uvarova <vbuvarova@gmail.com>
2023-06-23 00:22:18 +0000

    update files

--- hw2/config.py
+++ hw2/config.py
@@ -1,52 +1,40 @@
-# server host and port
 HOST = '127.0.0.1'
 PORT = 8001
 
-# pages
-MAPS = '/maps'
+# TEMPLATES
+MAIN_TEMPLATE = 'index.html'
+EVENTS_TEMPLATE = 'events.html'
+PLACES_TEMPLATE = 'maps.html'
+
+# PAGES
 EVENTS = '/events'
-PAGES = (MAPS, EVENTS)
+PLACES = '/maps'
+MAIN = '/'
+PAGES = EVENTS, PLACES
 
-# templates paths
-TEMPLATES = 'templates/'
-MAIN_PAGE = f'{TEMPLATES}index.html'
-EVENTS_TEMPLATE = f'{TEMPLATES}events.html'
-MAPS_TEMPLATE = f'{TEMPLATES}maps.html'
-ERROR_PAGE = f'{TEMPLATES}error.html'
+# db
+SELECT = 'SELECT * FROM {table}'
+INSERT = 'INSERT INTO {table} ({attrs}) VALUES ({values})'
+DELETE = 'DELETE FROM {table}'
+UPDATE = 'UPDATE {table} SET {data}'
+SELECT_ID = 'SELECT id FROM {table}'
+GET_TOKEN = 'SELECT token FROM token WHERE username=\'{username}\''
+EVENTS_ALL_ATTRS = ['id', 'event', 'date_s', 'description', 'location']
+EVENTS_REQ_ATTRS = ['event', 'description', 'location']
+POST_URL = f'http://{HOST}:{PORT}{EVENTS}?id='
 
-# HTTP headers
-CONTENT_LENGTH = 'Content-Length'
-CONTENT_TYPE = 'Content-Type', 'text/html'
-AUTH = 'Authorization'
-
-# HTTP server error codes
+# HTTTP codes
+OK = 200
 NOT_FOUND = 404
 FORBIDDEN = 403
 BAD_REQUEST = 400
-INTERNAL_ERROR = 500
-
-# HTTP OK codes
-OK = 200
-CREATED = 201
-NO_CONTENT = 204
-
-# other HTTP codes
 NOT_IMPLEMENTED = 501
+INTERNAL_ERR = 500
 
-# db requests
-RETURN_ID = " returning id"
-SELECTOR = 'SELECT id, event, date_s, description, location FROM events'
-GET_TOKEN = "SELECT token FROM token WHERE username=\'{username}\'"
-INSERT = 'INSERT INTO {table} ({keys}) VALUES ({values}) RETURNING id'
-UPDATE = 'UPDATE {table} SET {request}'
-DELETE = 'DELETE FROM {table} '
-EVENTS_REQ_ATTRS = ['event', 'description', 'location', 'date_s']
-EVENTS_ALL_ATTRS = ['id', 'event', 'date_s', 'description', 'location']
-MAPS_ATTRS = ['places']
+# CODING
+ENCODING = 'KOI8-R'
 
-# page str to byte coding
-CODING = 'KOI8-R'
-# maps things
+# maps consts
 URL = "https://static-maps.yandex.ru/1.x/?l=map&ll={}&size=650,450&z={}&l=map&pt=37.620070,55.753630,pmwtm1~37.64,55.76363,pmwtm99"
 places = {
     "lyceum": URL.format("39.98161706,43.40416754", "17"),
@@ -54,3 +42,7 @@ places = {
     "arena": URL.format("39.95343842,43.40890090", "17"),
     "karaganda": URL.format("73.08149641,49.81310069", "11")
 }
+
+# headers' names
+HEADER_LENGTH = 'Content-Length'
+HEADER_TYPE = 'Content-Type'
--- hw2/views.py
+++ hw2/views.py
@@ -1,56 +1,20 @@
-from config import *
-from requests import get
+from config import EVENTS_TEMPLATE, MAIN_TEMPLATE, PLACES_TEMPLATE
 
 
-def list_to_view(iterable: list):
-    res = ''
-    if iterable:
-        for item in iterable:
-            res = res + '<tr>'
-            for line in item:
-                res = res + f'<td>{line}</td>'
-            res = res + '</tr>'
-    else:
-        res = '<tr><td>No data given.</td></tr>'
-    return res
+def list_to_paragraphs(data: list):
+    return ''.join([f'<ul>{value}</ul>' for value in data]) if data else '<p>No data given.</p>'
 
 
 def events(events_data: dict) -> str:
-    with open(EVENTS_TEMPLATE, 'r') as template:
-        page = template.read()
-        return page.format(**events_data)
-
-
-def maps(maps_data: dict) -> str:
-    image_url = get_place(maps_data)
-    with open(MAPS_TEMPLATE, 'r') as template:
-        return template.read().format(image_url)
+    with open(EVENTS_TEMPLATE, 'r') as f:
+        return f.read().format(**events_data)
 
 
 def main_page() -> str:
-    with open(MAIN_PAGE, 'r') as template:
-        return template.read()
+    with open(MAIN_TEMPLATE, 'r') as f:
+        return f.read()
 
 
-def error_page(error: str) -> str:
-    with open(ERROR_PAGE, 'r') as template:
-        return template.read().format(error=error)
-
-
-def get_place(query: dict) -> str:
-    def g_image(url):
-        response = get(url, timeout=(5, 5))
-        if response.status_code != OK:
-            return BAD_REQUEST
-        elif response.status_code == OK:
-            return url
-
-    if query:
-        if query['places'] == 'arena':
-            return g_image(places['arena'])
-        elif query['places'] == 'sigma':
-            return g_image(places['sigma'])
-        elif query['places'] == 'lyceum':
-            return g_image(places['lyceum'])
-    else:
-        return g_image(places['karaganda'])
+def places(place: str) -> str:
+    with open(PLACES_TEMPLATE, 'r') as f:
+        return f.read().format(place)
